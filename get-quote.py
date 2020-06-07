c5c01a1c2ee1b68e029b37c306449555e9655262
Aakash634 <aakash634@gmail.com>
2020-06-07 17:55:39 +0530

    Signed-off-by: Aakash634 <aakash634@gmail.com> Renamed the primary function

--- get-quote.py
+++ get-quote.py
@@ -1,4 +1,4 @@
-def main1():
+def main2():
  print("Keep it logically awesome.")
 
   #f = open("quotes.txt")
@@ -8,4 +8,4 @@ def main1():
   #print(quotes)
 
 if __name__== "__main__":
-  main1()
+  main2()
